7170915cff7b6863b2e65ea2659c529eddb7f9b8
Félix Perez <felix2@hotmail.fr>
2019-01-08 14:56:08 +0100

    Controller part

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -2,10 +2,27 @@
 
 module Administration
   class ItemsController < AdministrationController
-    def index; end
+    def index
+      @items = Item.all
+    end
 
     def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        if @item.discount_percentage != 0
+      	@item.update(has_discount: true)
+      	flash[:success] = "L'item à bien été modifié"
+      	end
+      else
+        flash[:danger] = "Une erreur est survenue, l'item n'a pas pu être modifié"
+      end
       redirect_to administration_items_path
+    end
+
+    private
+
+    def item_params
+    	params.require(:item).permit(:discount_percentage)
     end
   end
 end
