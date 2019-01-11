9d1a431be21a37c44b21b5acd981ecb9cc4a0fe0
Félix Perez <felix2@hotmail.fr>
2019-01-11 17:31:03 +0100

    Add name column to Item table, create a new model called Category

---
+++ db/migrate/20190111162050_add_name_to_items.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddNameToItems < ActiveRecord::Migration[5.2]
+  def change
+    add_column :items, :name, :string, null: false, default: "default_name"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_150742) do
+ActiveRecord::Schema.define(version: 2019_01_11_162139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.float "original_price", null: false
@@ -21,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_01_05_150742) do
     t.integer "discount_percentage", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "default_name", null: false
   end
 
 end
