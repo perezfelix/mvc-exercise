e8a516a27454e55f007f9a98c4ae5af11259bc4b
Félix Perez <felix2@hotmail.fr>
2019-01-14 18:24:22 +0100

    Updating routes to have users and admin paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, path: 'admins'
+  devise_for :users, path: 'users'
   root 'administration/items#index'
 
   get '/home', to: 'home#landing_page'
