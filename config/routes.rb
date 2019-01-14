c921fa49f33c6982e8038627b5bb9d92968779c3
Félix Perez <felix2@hotmail.fr>
2019-01-14 21:34:04 +0100

    Setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins, path: 'admins'
-  devise_for :users, path: 'users'
+  devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions' }
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
   root 'administration/items#index'
 
   get '/home', to: 'home#landing_page'
 
   namespace 'administration' do
-    get '/', to: 'items#index'
+    root to: 'items#index'
 
     resources :items
   end
