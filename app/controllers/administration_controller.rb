6d10d6ec3880065120f4e31c485d0cfe65fcff26
Félix Perez <felix2@hotmail.fr>
2019-01-15 16:17:39 +0100

    Adding restriction for non-admin in administration controller

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class AdministrationController < ApplicationController
+  before_action :authenticate_admin!, except: [:index]
   layout "administration"
+  def index
+    super
+  end
 end
